fe8d9ffb7c5f9e5d7b7abb61c7ce56427a474a65
marcm <marcmondhaschen@gmail.com>
2024-08-19 22:05:38 -0700

    first pass at creating TeamsImport class to replace existing function code

--- nhlpd/teams.py
+++ nhlpd/teams.py
@@ -1,6 +1,91 @@
 import pandas as pd
-from api_query import fetch_json_data
-from mysql_db import db_import_login
+from .api_query import fetch_json_data
+from .mysql_db import db_import_login
+
+
+class TeamsImport:
+    teams_df = pd.DataFrame(columns=['id', 'franchiseId', 'fullName', 'leagueId', 'rawTricode', 'triCode'])
+
+    def __init__(self, teams_df=pd.DataFrame()):
+        self.teams_df = pd.concat([self.teams_df, teams_df])
+
+    @staticmethod
+    def updateDB(self):
+        """
+        Imports the teams dataframe contents into the local MySQL database
+
+        Parameters: none
+
+        Returns: True - Returns True upon completion
+        """
+        if len(self.teams_df) > 0:
+            cursor, db = db_import_login()
+
+            for index, row in self.teams_df.iterrows():
+                sql = "insert into teams_import (teamId, franchiseId, fullName, leagueId, triCode) " \
+                      "values (%s, %s, %s, %s, %s)"
+                val = (row['id'], row['franchiseId'], row['fullName'], row['leagueId'], row['triCode'])
+                cursor.execute(sql, val)
+
+            db.commit()
+            cursor.close()
+            db.close()
+        return True
+
+    @staticmethod
+    def clearDB():
+
+        cursor, db = db_import_login()
+
+        sql = "truncate table teams_import"
+        cursor.execute(sql)
+
+        db.commit()
+        cursor.close()
+        db.close()
+        return True
+
+    def queryDB(self):
+        """
+        Requests a list of NHL teams currently loaded in the local database. Updates the Teams object's teams_df with
+        the database's reply.
+
+        Parameters:
+
+        Returns:
+        """
+
+        teams_sql = "select teamId, franchiseId, fullName, leagueId, triCode from teams_import"
+
+        cursor, db = db_import_login()
+        db_teams_df = pd.read_sql(teams_sql, db)
+        self.teams_df = db_teams_df.fillna('')
+
+        return True
+
+    def queryNHL(self):
+        """
+        Requests a list of past & present NHL teams from the NHL API. Updates the Teams object's teams_df with
+        the NHL's reply.
+
+        Parameters:
+
+        Returns:
+        """
+
+        json_data = fetch_json_data('https://api.nhle.com/stats/rest/en/team')
+        api_teams_df = pd.json_normalize(json_data, record_path=['data'])
+        api_teams_df = api_teams_df.fillna('')
+        self.teams_df = pd.concat([self.teams_df, api_teams_df])
+
+        return True
+
+    def queryNHLupdateDB(self):
+        self.queryNHL()
+        self.clearDB()
+        self.updateDB(self)
+
+        return True
 
 
 def fetch_nhl_teams():
