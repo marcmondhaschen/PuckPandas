b5a1b3d4615378681c4418e936715b61641ac2cb
marcm <marcmondhaschen@gmail.com>
2024-08-25 20:53:56 -0700

    first pass at creating ShiftsImport class - still needs work to complete queryNHL, stubbed program code for log file

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import nhlpd
 
 # for now, main serves as a program script to test functions
 def main():
-    rosters = nhlpd.RostersImport()
-    result = rosters.queryNHLupdateDB()
+    shifts = nhlpd.ShiftsImport()
+    result = shifts.queryNHLupdateDB()
 
     return result
 
--- nhlpd/shifts.py
+++ nhlpd/shifts.py
@@ -2,117 +2,115 @@ from datetime import datetime
 import pandas as pd
 from .api_query import fetch_json_data
 from .mysql_db import db_import_login
+from .schedules import SchedulesImport
 
 
-def fetch_shifts_to_query():
-    """
-    Queries the local SQL database for a list of games to have their shifts queried from the NHL
-
-    Parameters: none
-
-    Returns: game_id_df - a Pandas Dataframe containing gameIds
-    """
-    games_sql = "select a.gameId from games_import as a left join shift_import_log as b " \
-                "on a.gameId = b.gameId where b.gameId is null;"
-
-    cursor, db = db_import_login()
-    game_id_df = pd.read_sql(games_sql, db)
-
-    return game_id_df
-
-
-def fetch_shifts():
-    """
-    Queries the NHL API for shift details by player for each game
-
-    Parameters: none
-
-    Returns:
-    """
-
-    game_id_df = fetch_shifts_to_query()
-
-    if len(game_id_df) == 0:
-        return False
-
-    for index, row in game_id_df.iterrows():
-        game_id = row['gameId']
-        shifts_load_check = False
-
-        url_prefix = 'https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId='
-        url_string = "{}{}".format(url_prefix, game_id)
-        json_data = fetch_json_data(url_string)
-
-        if len(json_data['data']) > 0:
-            shifts_df = pd.json_normalize(json_data, record_path=['data'])
-            master_shifts_df = master_shift_frame()
-            shifts_df = pd.concat([shifts_df, master_shifts_df])
-            shifts_df = transform_shifts_frame(shifts_df)
-            shifts_load_check = load_shifts_frame(shifts_df)
-
-        log_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-        log_df = pd.DataFrame(data=[[game_id, log_date, shifts_load_check]],
-                              columns=['gameId', 'logDate', 'checked'])
-        log_df = log_df.fillna('')
-        update_shift_log(log_df)
-
-    return True
-
-
-def master_shift_frame():
-    """ Manually builds an empty Pandas Dataframe with columns consistent with shift statistics
-
-    Parameters: none
-
-    Returns: shifts_df - an empty Pandas Dataframe with columns consistent with shift statistics
-    """
+class ShiftsImport:
     shifts_df = pd.DataFrame(columns=['id', 'detailCode', 'duration', 'endTime', 'eventDescription', 'eventDetails',
-                                      'firstName', 'gameId', 'hexValue', 'lastName', 'period', 'playerId',
-                                      'shiftNumber', 'startTime', 'teamAbbrev', 'teamId', 'teamName', 'typeCode'])
+                                      'eventNumber', 'firstName', 'gameId', 'hexValue', 'lastName', 'period',
+                                      'playerId', 'shiftNumber', 'startTime', 'teamAbbrev', 'teamId', 'teamName',
+                                      'typeCode'])
 
-    return shifts_df
+    def __init__(self, shifts_df=pd.DataFrame()):
+        self.shifts_df = pd.concat([self.shifts_df, shifts_df])
 
+    @staticmethod
+    def updateDB(self):
+        cursor, db = db_import_login()
 
-def transform_shifts_frame(shifts_df):
-    """
-    Transforms shifts import data so that it's ready to import into the database
+        for index, row in self.shifts_df.iterrows():
+            sql = 'insert into shifts_import (id, detailCode, duration, endTime, eventDescription, eventDetails, ' \
+                  'eventNumber, firstName, gameId, hexValue, lastName, period, playerId, shiftNumber, startTime, ' \
+                  'teamAbbrev, teamId, teamName, typeCode) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ' \
+                  '%s, %s, %s, %s, %s, %s, %s)'
+            val = [row['id'], row['detailCode'], row['duration'], row['endTime'], row['eventDescription'],
+                   row['eventDetails'], row['eventNumber'], row['firstName'], row['gameId'], row['hexValue'],
+                   row['lastName'], row['period'], row['playerId'], row['shiftNumber'], row['startTime'],
+                   row['teamAbbrev'], row['teamId'], row['teamName'], row['typeCode']]
 
-    Parameters: shifts_df - a Dataframe that requires transformation before it's ready to be loaded to the database
+            cursor.execute(sql, val)
 
-    Returns: shifts_df - a Dataframe object that is ready to be loaded into the database
-    """
-    shifts_df = shifts_df.fillna('')
-    return shifts_df
+        # tidy up the cursors
+        db.commit()
+        cursor.close()
+        db.close()
 
+        return True
 
-def load_shifts_frame(shifts_df):
-    """
-    Inserts shift records into the shifts_import table
+    @staticmethod
+    def clearDB():
+        cursor, db = db_import_login()
 
-    Parameters: shifts_df - a DataFrame with shifts played for each player and gameId
+        sql = "truncate table shifts_import"
+        cursor.execute(sql)
 
-    Returns: True - returns True upon completion
-    """
-    cursor, db = db_import_login()
+        db.commit()
+        cursor.close()
+        db.close()
+        return True
 
-    for index, row in shifts_df.iterrows():
-        sql = 'insert into shifts_import (id, detailCode, duration, endTime, eventDescription, eventDetails, ' \
-              'eventNumber, firstName, gameId, hexValue, lastName, period, playerId, shiftNumber, startTime, ' \
-              'teamAbbrev, teamId, teamName, typeCode) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ' \
-              '%s, %s, %s, %s, %s, %s, %s)'
-        val = [row['id'], row['detailCode'], row['duration'], row['endTime'], row['eventDescription'],
-               row['eventDetails'], row['eventNumber'], row['firstName'], row['gameId'], row['hexValue'],
-               row['lastName'], row['period'], row['playerId'], row['shiftNumber'], row['startTime'], row['teamAbbrev'],
-               row['teamId'], row['teamName'], row['typeCode']]
+    def queryDB(self, gameid='', playerid='', teamid=''):
+        shifts_sql = "select id, detailCode, duration, endTime, eventDescription, eventDetails, eventNumber, " \
+                     "firstName, gameId, hexValue, lastName, period, playerId, shiftNumber, startTime, teamAbbrev, " \
+                     "teamId, teamName, typeCode from shifts_import where id > 0 "
 
-        cursor.execute(sql, val)
+        gameid_sql = playerid_sql = teamid_sql = ''
 
-    # tidy up the cursors
-    db.commit()
-    cursor.close()
-    db.close()
+        if gameid != '':
+            gameid_sql = "and gameId = " + gameid + " "
+        if playerid != '':
+            playerid_sql = "and playerId = " + playerid + " "
+        if teamid != '':
+            teamid_sql = "and teamId = " + teamid + " "
 
-    return True
+        shifts_sql = "{}{}{}{}".format(shifts_sql, gameid_sql, playerid_sql, teamid_sql)
+
+        cursor, db = db_import_login()
+        shifts_df = pd.read_sql(shifts_sql, db)
+        self.shifts_df = shifts_df.fillna('')
+
+        db.commit()
+        cursor.close()
+        db.close()
+
+        return True
+
+    def queryNHL(self, gameid=''):
+        schedules = SchedulesImport()
+        schedules.queryDB()
+
+        if len(schedules.schedules_df) == 0:
+            return False
+
+        for index, row in schedules.schedules_df.iterrows():
+            game_id = row['gameId']
+            shifts_load_check = False
+
+            url_prefix = 'https://api.nhle.com/stats/rest/en/shiftcharts?cayenneExp=gameId='
+            url_string = "{}{}".format(url_prefix, game_id)
+            json_data = fetch_json_data(url_string)
+
+            if len(json_data['data']) > 0:
+                shifts_df = pd.json_normalize(json_data, record_path=['data'])
+                master_shifts_df = master_shift_frame()
+                shifts_df = pd.concat([shifts_df, master_shifts_df])
+                shifts_df = transform_shifts_frame(shifts_df)
+                shifts_load_check = load_shifts_frame(shifts_df)
+
+            log_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+            log_df = pd.DataFrame(data=[[game_id, log_date, shifts_load_check]],
+                                  columns=['gameId', 'logDate', 'checked'])
+            log_df = log_df.fillna('')
+            # update_shift_log(log_df)
+
+        return True
+
+    def queryNHLupdateDB(self):
+        self.queryNHL()
+        self.clearDB()
+        self.updateDB(self)
+
+        return True
 
 
 def update_shift_log(log_df):
@@ -137,16 +135,3 @@ def update_shift_log(log_df):
     db.close()
 
     return True
-
-
-def etl_shifts():
-    """
-    Queries the local database for a list of games with no shift data, queries that data from the NHL, and loads it
-    to the local database
-
-    Parameters: none
-
-    Returns: checkvar - a bool to verify that function has run
-    """
-    check_var = fetch_shifts()
-    return check_var
