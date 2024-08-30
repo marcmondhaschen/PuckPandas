b95c80265bad16c8c99f08f5558080d282ec9992
marcm <marcmondhaschen@gmail.com>
2024-08-29 22:57:36 -0700

    minor syntax fixes

--- nhlpd/import_table_update_log.py
+++ nhlpd/import_table_update_log.py
@@ -10,10 +10,10 @@ class ImportTableUpdateLog:
 
     @staticmethod
     def updateDB(self):
-        if len(self.table_update_log) & self.table_update_log['tableName'] > 0:
+        if (len(self.table_update_log) > 0) and ('tableName' in self.table_update_log):
             cursor, db = db_import_login()
 
-            for index, row in self.seasons_df.iterrows():
+            for index, row in self.table_update_log.iterrows():
                 sql = "insert into table_update_log (tableName, lastDateUpdated, updateFound) values (%s, %s, %s)"
                 val = (row['tableName'], row['lastDateUpdated'], row['updateFound'])
                 cursor.execute(sql, val)
@@ -24,12 +24,15 @@ class ImportTableUpdateLog:
 
         return True
 
-    def queryDB(self, table_name=''):
+    def queryDB(self, table_name='', update_found=1):
         cursor, db = db_import_login()
 
-        prefix_sql = "select tableName, lastDateUpdated, updateFound from table_update_log where tableName = '"
-        suffix_sql = "'"
-        update_log_sql = "{}{}{}".format(prefix_sql, table_name, suffix_sql)
+        prefix_sql = "select tableName, max(lastDateUpdated) as lastDateUpdated, " \
+                     "updateFound from table_update_log where tableName = '"
+        suffix_sql = "' and updateFound = "
+        suffix2_sql = " group by tableName, updateFound"
+        update_log_sql = "{}{}{}{}{}".format(prefix_sql, table_name, suffix_sql, update_found, suffix2_sql)
+
         self.table_update_log = pd.read_sql(update_log_sql, db)
         self.table_update_log.fillna('', inplace=True)
 
