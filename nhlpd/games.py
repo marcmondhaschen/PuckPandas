e9d037a21fa1e41f12e4ce4e744709629f2b01d2
marcm <marcmondhaschen@gmail.com>
2024-08-30 22:44:51 -0700

    renamed Schedules to Games.Py, added triCode and SeasonId overrides, stubbed in GamesImportLog logging - still needs testing

--- nhlpd/games.py
+++ nhlpd/games.py
@@ -5,6 +5,7 @@ from .api_query import fetch_json_data
 from .mysql_db import db_import_login
 from .seasons import SeasonsImport
 from .import_table_update_log import ImportTableUpdateLog
+from .games_import_log import GamesImportLog
 
 
 class SchedulesImport:
@@ -18,9 +19,15 @@ class SchedulesImport:
         self.schedules_df = pd.concat([self.schedules_df, schedules_df])
 
     @staticmethod
-    def updateDB(self):
+    def updateDB(self, tri_code='', season_id=''):
         if len(self.schedules_df) > 0:
             cursor, db = db_import_login()
+
+            if tri_code != '':
+                self.schedules_df = self.schedules_df[self.schedules_df['triCode'] == tri_code]
+
+            if season_id != '':
+                self.schedules_df = self.schedules_df[self.schedules_df['seasonId'] == season_id]
 
             for index, row in self.schedules_df.iterrows():
                 sql = "insert into games_import (gameId, seasonId, gameType, gameDate, venue, neutralSite, " \
@@ -35,6 +42,8 @@ class SchedulesImport:
                        row['periodDescriptor.periodType'], row['gameOutcome.lastPeriodType'])
                 cursor.execute(sql, val)
 
+                log = GamesImportLog()
+
             db.commit()
             cursor.close()
             db.close()
@@ -45,10 +54,20 @@ class SchedulesImport:
         return True
 
     @staticmethod
-    def clearDB():
+    def clearDB(tri_code='', season_id=''):
         cursor, db = db_import_login()
 
-        sql = "truncate table games_import"
+        if tri_code == '' and season_id == '':
+            sql = "truncate table games_import"
+        else:
+            sql_prefix = "delete from games_import where gameId > 0 "
+            sql_middle = ""
+            sql_suffix = ""
+            if tri_code != '':
+                sql_middle = "and triCode = " + tri_code + " "
+            if season_id != '':
+                sql_suffix = "and seasonId = " + season_id + " "
+            sql = "{}{}{}".format(sql_prefix, sql_middle, sql_suffix)
         cursor.execute(sql)
 
         db.commit()
@@ -56,14 +75,24 @@ class SchedulesImport:
         db.close()
         return True
 
-    def queryDB(self):
-        schedules_sql = "select gameId, seasonId, gameType, gameDate, venue, neutralSite, startTimeUTC, " \
-                        "venueUTCOffset, venueTimezone, gameState, gameScheduleState, awayTeam, awayTeamSplitSquad, " \
-                        "awayTeamScore, homeTeam, homeTeamSplitSquad, homeTeamScore, periodType, gameOutcome from " \
-                        "games_import"
+    def queryDB(self, tri_code='', season_id=''):
+        sql_prefix = "select gameId, seasonId, gameType, gameDate, venue, neutralSite, startTimeUTC, venueUTCOffset, " \
+                     "venueTimezone, gameState, gameScheduleState, awayTeam, awayTeamSplitSquad, awayTeamScore, " \
+                     "homeTeam, homeTeamSplitSquad, homeTeamScore, periodType, gameOutcome from games_import where " \
+                     "gameId > 0 "
+        sql_middle = ""
+        sql_suffix = ""
+
+        if tri_code != '':
+            sql_middle = "and  triCode = " + tri_code + " "
+
+        if season_id != '':
+            sql_suffix = "and seasonId = " + season_id + " "
+
+        sql = "{}{}{}".format(sql_prefix, sql_middle, sql_suffix)
 
         cursor, db = db_import_login()
-        schedules_df = pd.read_sql(schedules_sql, db)
+        schedules_df = pd.read_sql(sql, db)
         self.schedules_df = schedules_df.fillna('')
 
         db.commit()
@@ -72,15 +101,15 @@ class SchedulesImport:
 
         return True
 
-    def queryNHL(self):
-        cursor, db = db_import_login()
-
+    def queryNHL(self, tri_code='', season_id=''):
         seasons = SeasonsImport()
         seasons.queryDB()
 
-        db.commit()
-        cursor.close()
-        db.close()
+        if tri_code != '':
+            seasons.seasons_df = seasons.seasons_df[seasons.seasons_df['triCode'] == tri_code]
+
+        if season_id != '':
+            seasons.seasons_df = seasons.seasons_df[seasons.seasons_df['seasonId'] == season_id]
 
         schedules_df = pd.DataFrame()
 
@@ -105,9 +134,9 @@ class SchedulesImport:
 
         return True
 
-    def queryNHLupdateDB(self):
-        self.queryNHL()
-        self.clearDB()
-        self.updateDB(self)
+    def queryNHLupdateDB(self, tri_code='', season_id=''):
+        self.queryNHL(tri_code, season_id)
+        self.clearDB(tri_code, season_id)
+        self.updateDB(self, tri_code, season_id)
 
         return True
