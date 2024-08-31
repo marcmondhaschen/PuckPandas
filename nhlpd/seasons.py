66bce177bdbdbfefd7748a201a72b7bf2a3dd3be
marcm <marcmondhaschen@gmail.com>
2024-08-30 19:23:34 -0700

    added overrides to SeasonsImport object - needs testing

--- nhlpd/seasons.py
+++ nhlpd/seasons.py
@@ -5,6 +5,7 @@ from .mysql_db import db_import_login
 from .teams import TeamsImport
 from .import_table_update_log import ImportTableUpdateLog
 
+
 class SeasonsImport:
     seasons_df = pd.DataFrame(columns=['triCode', 'seasonId'])
 
@@ -12,9 +13,12 @@ class SeasonsImport:
         self.seasons_df = pd.concat([self.seasons_df, seasons_df])
 
     @staticmethod
-    def updateDB(self):
+    def updateDB(self, tri_code=''):
         if len(self.seasons_df) > 0:
             cursor, db = db_import_login()
+
+            if tri_code != '':
+                self.teams_df = self.teams_df[self.teams_df['triCode'] == tri_code]
 
             for index, row in self.seasons_df.iterrows():
                 sql = "insert into team_seasons_import (triCode, seasonId) values (%s, %s)"
@@ -31,10 +35,13 @@ class SeasonsImport:
         return True
 
     @staticmethod
-    def clearDB():
+    def clearDB(tri_code):
         cursor, db = db_import_login()
 
-        sql = "truncate table team_seasons_import"
+        if tri_code == '':
+            sql = "truncate table team_seasons_import"
+        else:
+            sql = "delete from team_seasons_import where triCode = " + tri_code
         cursor.execute(sql)
 
         db.commit()
@@ -42,11 +49,17 @@ class SeasonsImport:
         db.close()
         return True
 
-    def queryDB(self):
-        seasons_sql = "select triCode, seasonId from team_seasons_import"
+    def queryDB(self, tri_code=''):
+        sql_prefix = "select triCode, seasonId from team_seasons_import"
+        sql_suffix = ""
+
+        if tri_code != '':
+            sql_suffix = "where triCode = " + tri_code
+
+        sql = "{}{}".format(sql_prefix, sql_suffix)
 
         cursor, db = db_import_login()
-        seasons_df = pd.read_sql(seasons_sql, db)
+        seasons_df = pd.read_sql(sql, db)
         self.seasons_df = seasons_df.fillna('')
 
         db.commit()
@@ -55,7 +68,7 @@ class SeasonsImport:
 
         return True
 
-    def queryNHL(self):
+    def queryNHL(self, tri_code=''):
         teams = TeamsImport()
         teams.queryDB()
 
@@ -74,11 +87,14 @@ class SeasonsImport:
 
         self.seasons_df = team_seasons_df
 
+        if tri_code != '':
+            self.seasons_df = self.seasons_df[self.seasons_df['triCode'] == tri_code]
+
         return True
 
-    def queryNHLupdateDB(self):
-        self.queryNHL()
-        self.clearDB()
-        self.updateDB(self)
+    def queryNHLupdateDB(self, tri_code=''):
+        self.queryNHL(tri_code)
+        self.clearDB(tri_code)
+        self.updateDB(self, tri_code)
 
         return True
