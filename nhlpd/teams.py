fa0b9a8089f1b8b095026adc206b9ad73d045757
marcm <marcmondhaschen@gmail.com>
2024-08-30 19:11:58 -0700

    added overrides to TeamsImport object - needs testing

--- nhlpd/teams.py
+++ nhlpd/teams.py
@@ -12,13 +12,16 @@ class TeamsImport:
         self.teams_df = pd.concat([self.teams_df, teams_df])
 
     @staticmethod
-    def updateDB(self):
+    def updateDB(self, tri_code=''):
         cursor, db = db_import_login()
+
+        if tri_code != '':
+            self.teams_df = self.teams_df[self.teams_df['triCode'] == tri_code]
 
         if len(self.teams_df) > 0:
             for index, row in self.teams_df.iterrows():
                 sql = "insert into teams_import (teamId, franchiseId, fullName, leagueId, triCode) " \
-                      "values (%s, %s, %s, %s, %s)"
+                      "values (%s, %s, %s, %s, %s) "
                 val = (row['id'], row['franchiseId'], row['fullName'], row['leagueId'], row['triCode'])
                 cursor.execute(sql, val)
 
@@ -31,22 +34,33 @@ class TeamsImport:
         return True
 
     @staticmethod
-    def clearDB():
+    def clearDB(tri_code=''):
         cursor, db = db_import_login()
 
-        sql = "truncate table teams_import"
+        if tri_code == '':
+            sql = "truncate table teams_import"
+        else:
+            sql = "delete from teams_import where triCode = " + tri_code
+
         cursor.execute(sql)
 
         db.commit()
         cursor.close()
         db.close()
+
         return True
 
-    def queryDB(self):
-        teams_sql = "select teamId, franchiseId, fullName, leagueId, triCode from teams_import"
+    def queryDB(self, tri_code=''):
+        sql_prefix = "select teamId, franchiseId, fullName, leagueId, triCode from teams_import "
+        sql_suffix = ""
+
+        if tri_code != '':
+            sql_suffix = "where triCode = " + tri_code
+
+        sql = "{}{}".format(sql_prefix, sql_suffix)
 
         cursor, db = db_import_login()
-        teams_df = pd.read_sql(teams_sql, db)
+        teams_df = pd.read_sql(sql, db)
         self.teams_df = teams_df.fillna('')
 
         db.commit()
@@ -55,17 +69,20 @@ class TeamsImport:
 
         return True
 
-    def queryNHL(self):
+    def queryNHL(self, tri_code=''):
         json_data = fetch_json_data('https://api.nhle.com/stats/rest/en/team')
         api_teams_df = pd.json_normalize(json_data, record_path=['data'])
         api_teams_df = api_teams_df.fillna('')
         self.teams_df = pd.concat([self.teams_df, api_teams_df])
 
+        if tri_code != '':
+            self.teams_df = self.teams_df[self.teams_df['triCode'] == tri_code]
+
         return True
 
-    def queryNHLupdateDB(self):
-        self.queryNHL()
-        self.clearDB()
-        self.updateDB(self)
+    def queryNHLupdateDB(self, tri_code=''):
+        self.queryNHL(tri_code)
+        self.clearDB(tri_code)
+        self.updateDB(self, tri_code)
 
         return True
