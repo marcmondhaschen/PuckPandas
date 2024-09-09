b55c3d5c617daa6616ae300497cab2a4ae4202a1
marcm <marcmondhaschen@gmail.com>
2024-09-09 14:05:59 -0700

    added alchemy_db.py which contains same two db connections as mysql_db.py, but using the SQLAlchemy library instead of the mysql.connector library

---
+++ alchemy_db.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+def dba_import_login():
+    """
+    Uses dotenv library and local .env credentials to provide SQLAlchemy database access
+
+    Parameters:
+
+    Returns: engine - a SQLAlchemy Engine object
+    """
+    load_dotenv()
+
+    dialect = "mysql"
+    driver = "mysqlconnector"
+    username = os.environ['DB_IMPORT_USER']
+    password = os.environ['DB_IMPORT_PASSWORD']
+    server = os.environ['DB_IMPORT_HOST']
+    port = os.environ['DB_IMPORT_PORT']
+    schema = os.environ['DB_IMPORT_SCHEMA']
+
+    connection_string = "{}+{}://{}:{}@{}:{}/{}".format(dialect, driver, username, password, server, port, schema)
+    engine = create_engine(connection_string)
+
+    return engine
+
+
+def dba_test_login():
+    """
+    Uses dotenv library and local .env credentials to provide SQLAlchemy database access
+
+    Parameters:
+
+    Returns: engine - a SQLAlchemy Engine object
+    """
+    load_dotenv()
+
+    dialect = "mysql"
+    driver = "mysqlconnector"
+    username = os.environ['DB_TEST_USER']
+    password = os.environ['DB_TEST_PASSWORD']
+    server = os.environ['DB_TEST_HOST']
+    port = os.environ['DB_TEST_PORT']
+    schema = os.environ['DB_TEST_SCHEMA']
+
+    connection_string = "{}+{}://{}:{}@{}:{}/{}".format(dialect, driver, username, password, server, port, schema)
+    engine = create_engine(connection_string)
+
+    return engine
