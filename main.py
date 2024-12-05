acfa338f9c194320451b374b04791b73a6e86604
marcm <marcmondhaschen@gmail.com>
2024-12-05 12:24:13 -0800

    updated main to support testing of players.py

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import nhlpd
 
 # for now, main serves as a program script to test functions
 if __name__ == "__main__":
-    game_center = nhlpd.GameCenterImport()
-    game_center = game_center.queryNHLupdateDB(game_id=2023021019)
-    game_center_copy = nhlpd.GameCenterImport()
-    game_center_copy.queryDB(game_id=2023021019)
-    print(game_center_copy.scratches.scratches_df)
+    skater_current = nhlpd.PlayersImport()
+    skater_retired = nhlpd.PlayersImport()
+    goalie_current = nhlpd.PlayersImport()
+    goalie_retired = nhlpd.PlayersImport()
+
+    # Connor Bedard
+    skater_current = skater_current.queryNHLupdateDB(player_id=8484144)
+    # Jay Bouwmeester
+    skater_retired = skater_retired.queryNHLupdateDB(player_id=8470151)
+    # Thatcher Demko
+    goalie_current = goalie_current.queryNHLupdateDB(player_id=8477967)
+    # Curtis Joseph
+    goalie_retired = goalie_retired.queryNHLupdateDB(player_id=8448382)
