d774a0db13f923d594f4eb56d77c5af90c2b28b0
marcm <marcmondhaschen@gmail.com>
2025-01-01 17:02:41 -0800

    sqlalchemy conversion

--- nhlpd/shifts.py
+++ nhlpd/shifts.py
@@ -1,34 +1,31 @@
 import pandas as pd
 import nhlpd
+from sqlalchemy import text
 
 """ shift details first appear in the NHL's API set in the 20102011 season """
 class ShiftsImport:
     def __init__(self, game_id):
-        self.shifts_df = pd.DataFrame(columns=['id', 'detailCode', 'duration', 'endTime', 'eventDescription',
-                                               'eventDetails', 'eventNumber', 'firstName', 'gameId', 'hexValue',
-                                               'lastName', 'period', 'playerId', 'shiftNumber', 'startTime',
-                                               'teamAbbrev', 'teamId', 'teamName', 'typeCode'])
         self.game_id = game_id
+        self.table_columns = ['id', 'detailCode', 'duration', 'endTime', 'eventDescription', 'eventDetails',
+                              'eventNumber', 'firstName', 'gameId', 'hexValue', 'lastName', 'period', 'playerId',
+                              'shiftNumber', 'startTime', 'teamAbbrev', 'teamId', 'teamName', 'typeCode']
+        self.shifts_df = pd.DataFrame()
+        self.query_db()
 
     def update_db(self):
         shifts_found = 0
         if self.shifts_df.size > 0:
             shifts_found = 1
 
-            cursor, db = nhlpd.db_import_login()
-            for index, row in self.shifts_df.iterrows():
-                sql = 'insert into shifts_import (id, detailCode, duration, endTime, eventDescription, eventDetails, ' \
-                      'eventNumber, firstName, gameId, hexValue, lastName, period, playerId, shiftNumber, startTime, ' \
-                      'teamAbbrev, teamId, teamName, typeCode) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ' \
-                      '%s, %s, %s, %s, %s, %s, %s)'
-                val = [row['id'], row['detailCode'], row['duration'], row['endTime'], row['eventDescription'],
-                       row['eventDetails'], row['eventNumber'], row['firstName'], row['gameId'], row['hexValue'],
-                       row['lastName'], row['period'], row['playerId'], row['shiftNumber'], row['startTime'],
-                       row['teamAbbrev'], row['teamId'], row['teamName'], row['typeCode']]
-                cursor.execute(sql, val)
-            db.commit()
-            cursor.close()
-            db.close()
+            engine = nhlpd.dba_import_login()
+            sql = 'insert into shifts_import (id, detailCode, duration, endTime, eventDescription, eventDetails, ' \
+                  'eventNumber, firstName, gameId, hexValue, lastName, period, playerId, shiftNumber, startTime, ' \
+                  'teamAbbrev, teamId, teamName, typeCode) values (:id, :detailCode, :duration, :endTime, ' \
+                  ':eventDescription, :eventDetails, :eventNumber, :firstName, :gameId, :hexValue, :lastName, ' \
+                  ':period, :playerId, :shiftNumber, :startTime, :teamAbbrev, :teamId, :teamName, :typeCode)'
+            params = self.shifts_df.to_dict('records')
+            with engine.connect() as conn:
+                conn.execute(text(sql), parameters=params)
 
         log = nhlpd.GamesImportLog(game_id=self.game_id, shifts_found=shifts_found)
         log.update_db()
@@ -36,28 +33,26 @@ class ShiftsImport:
         return True
 
     def clear_db(self):
-        cursor, db = nhlpd.db_import_login()
+        engine = nhlpd.dba_import_login()
         sql = "delete from shifts_import where gameId =" + str(self.game_id)
-        cursor.execute(sql)
-        db.commit()
-        cursor.close()
-        db.close()
+        with engine.connect() as conn:
+            conn.execute(text(sql))
 
         return True
 
     def query_db(self):
-        cursor, db = nhlpd.db_import_login()
+        engine = nhlpd.dba_import_login()
         sql = "select id, detailCode, duration, endTime, eventDescription, eventDetails, eventNumber, firstName, " \
               "gameId, hexValue, lastName, period, playerId, shiftNumber, startTime, teamAbbrev, teamId, teamName, " \
               "typeCode from shifts_import where gameId = " + str(self.game_id)
-        shifts_df = pd.read_sql(sql, db)
-        db.commit()
-        cursor.close()
-        db.close()
+        shifts_df = pd.read_sql_query(sql, engine)
+        engine.dispose()
 
-        self.shifts_df = self.shifts_df.head(0)
-        self.shifts_df = pd.concat([self.shifts_df, shifts_df])
-        self.shifts_df.fillna('', inplace=True)
+        if shifts_df.size > 0:
+            shifts_df.fillna('', inplace=True)
+            self.shifts_df = shifts_df
+
+        self.shifts_df.reindex(columns=self.table_columns)
 
         return True
 
@@ -68,9 +63,9 @@ class ShiftsImport:
 
         if len(json_data['data']) > 0:
             shifts_df = pd.json_normalize(json_data, record_path=['data'])
-            self.shifts_df = self.shifts_df.head(0)
-            self.shifts_df = pd.concat([self.shifts_df, shifts_df])
-            self.shifts_df.fillna('', inplace=True)
+            shifts_df.reindex(columns=self.table_columns)
+            shifts_df.fillna('', inplace=True)
+            self.shifts_df = shifts_df
 
         return True
 
