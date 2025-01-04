4cb74bff7d3642ccf949578ffa63b8ed5f6b0b6b
marcm <marcmondhaschen@gmail.com>
2025-01-04 08:49:41 -0800

    fixed another fillna warning

--- nhlpd/teams.py
+++ nhlpd/teams.py
@@ -52,8 +52,9 @@ class TeamsImport:
         if json_data != {}:
             teams_df = pd.json_normalize(json_data, record_path=['data'])
             teams_df.rename(columns={'id': 'teamId'}, inplace=True)
-            # teams_df.fillna(0, inplace=True)
-            self.teams_df = teams_df.reindex(columns=self.table_columns)
+            teams_df = teams_df.reindex(columns=self.table_columns)
+            teams_df.fillna(0, inplace=True)
+            self.teams_df = teams_df
 
         if tri_code != '':
             self.teams_df = self.teams_df[self.teams_df['triCode'] == tri_code]
