edb14bd8cc65bf8bd0f2d2ae8b077a180dadb47b
marcm <marcmondhaschen@gmail.com>
2025-01-07 13:11:01 -0800

    simplified main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import puckpandas
+import puckpandas as pp
 
 # for now, main serves as a program script to test functions
 if __name__ == "__main__":
-    scheduler = puckpandas.Scheduler()
+    scheduler = pp.Scheduler()
     scheduler.poll_nhl()
     print("the end!")
