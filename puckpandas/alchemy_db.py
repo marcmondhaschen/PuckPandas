804b7c149a0daed1d314a3966ded6906ada40992
marcm <marcmondhaschen@gmail.com>
2025-01-07 19:47:50 -0800

    added prod_tx and prod database logins

--- puckpandas/alchemy_db.py
+++ puckpandas/alchemy_db.py
@@ -13,7 +13,8 @@ def dba_import_login():
     port = os.environ['DB_IMPORT_PORT']
     schema = os.environ['DB_IMPORT_SCHEMA']
 
-    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password, server, port, schema)
+    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
+                                                                        server, port, schema)
     engine = create_engine(connection_string, poolclass=NullPool)
 
     return engine
@@ -30,7 +31,43 @@ def dba_test_login():
     port = os.environ['DB_TEST_PORT']
     schema = os.environ['DB_TEST_SCHEMA']
 
-    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password, server, port, schema)
+    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
+                                                                        server, port, schema)
     engine = create_engine(connection_string)
 
     return engine
+
+def db_prod_tx_login():
+    load_dotenv()
+
+    dialect = "mysql"
+    driver = "mysqlconnector"
+    username = os.environ['DB_PROD_TX_USER']
+    password = os.environ['DB_PROD_TX_PASSWORD']
+    server = os.environ['DB_PROD_TX_HOST']
+    port = os.environ['DB_PROD_TX_PORT']
+    schema = os.environ['DB_PROD_TX_SCHEMA']
+
+    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
+                                                                        server, port, schema)
+    engine = create_engine(connection_string)
+
+    return engine
+
+def db_prod_login():
+    load_dotenv()
+
+    dialect = "mysql"
+    driver = "mysqlconnector"
+    username = os.environ['DB_PROD_USER']
+    password = os.environ['DB_PROD_PASSWORD']
+    server = os.environ['DB_PROD_HOST']
+    port = os.environ['DB_PROD_PORT']
+    schema = os.environ['DB_PROD_SCHEMA']
+
+    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
+                                                                        server, port, schema)
+    engine = create_engine(connection_string)
+
+    return engine
+
