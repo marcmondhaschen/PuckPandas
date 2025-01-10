c9a886c8120139a7f548d51a06b34f5062dba4e6
marcm <marcmondhaschen@gmail.com>
2025-01-09 19:52:08 -0800

    shifts before 2010 season aren't present and shouldn't query

--- puckpandas/games_import_log.py
+++ puckpandas/games_import_log.py
@@ -136,10 +136,12 @@ class GamesImportLog:
 
         return games_open_work_df
 
+
     @staticmethod
     def shifts_not_queried():
+        """ shift details first appear in the NHL's API set in the 20102011 season """
         engine = puckpandas.dba_import_login()
-        sql = "select gameId from puckpandas_import.games_import_log where shiftsFound is Null"
+        sql = "select gameId from puckpandas_import.games_import_log where shiftsFound is Null and gameId >= 2010010000"
         shifts_open_work_df = pd.read_sql_query(sql, engine)
         engine.dispose()
 
@@ -147,10 +149,11 @@ class GamesImportLog:
 
     @staticmethod
     def shifts_played_recently(start_date, stop_date):
+        """ shift details first appear in the NHL's API set in the 20102011 season """
         engine = puckpandas.dba_import_login()
         sql = "select a.gameId from puckpandas_import.games_import_log as a join puckpandas_import.games_import as b " \
               "on a.gameId = b.gameId where a.shiftsFound = 0 and b.gameDate between '" + str(start_date) + "' and '" \
-              + str(stop_date) + "'"
+              + str(stop_date) + "' a.gameId >= 2010010000"
         shifts_open_work_df = pd.read_sql_query(sql, engine)
         engine.dispose()
 
