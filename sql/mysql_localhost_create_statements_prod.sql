d171ad0e569883451f26232d9828eaf9f24b2555
marcm <marcmondhaschen@gmail.com>
2025-01-13 16:05:22 -0800

    temp file

---
+++ sql/mysql_localhost_create_statements_prod.sql
@@ -0,0 +1,87 @@
+# create coaches
+
+# create games
+
+# create game_coaches
+
+# create game_faceoffs
+
+# create game_giveaway_takeaway
+
+# create game_goals
+
+# create game_hits
+
+# create game_linesmen
+
+# create game_penalties
+
+# create game_plays
+
+# create game_play_timings
+
+# create game_progress
+
+# create game_referees
+
+# create game_roster_spots
+
+# create game_scores
+
+# create game_scratches
+
+# create game_series
+
+# create game_series_groups
+
+# create game_shots
+
+# create game_stoppages
+
+# create game_team_stats
+
+# create game_tv_broadcasts
+
+# create game_videos
+
+# create goalie_career_totals
+
+# create goalie_seasons
+
+# create leagues
+
+# create linesmen
+
+# create play_type_codes
+
+# create player_awards
+
+# create player_bios
+
+# create player_drafts
+
+# create player_headshots
+
+# create player_statuses
+
+# create referees
+
+# create shifts_goals
+
+# create shifts
+
+# create skater_career_totals
+
+# create skater_seasons
+
+# create team_art
+
+# create team_rosters
+
+# create team_seasons
+
+# create teams
+
+# create trophies
+
+# create venues
