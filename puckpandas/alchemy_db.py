7829f0172d5e305b8c7b2b94c603cee4c33887cb
marcm <marcmondhaschen@gmail.com>
2025-03-12 09:47:21 -0700

    added new database users and test switches to function interfaces

--- puckpandas/alchemy_db.py
+++ puckpandas/alchemy_db.py
@@ -2,16 +2,24 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, NullPool
 
-def dba_import_login():
-    load_dotenv()
 
+def dba_import_login(test=0):
+    load_dotenv()
     dialect = "mysql"
     driver = "mysqlconnector"
-    username = os.environ['DB_IMPORT_USER']
-    password = os.environ['DB_IMPORT_PASSWORD']
-    server = os.environ['DB_IMPORT_HOST']
-    port = os.environ['DB_IMPORT_PORT']
-    schema = os.environ['DB_IMPORT_SCHEMA']
+
+    if test == 0:
+        server = os.environ['DB_IMP_HOST']
+        port = os.environ['DB_IMP_PORT']
+        schema = os.environ['DB_IMP_SCHEMA']
+        username = os.environ['DB_IMP_USER']
+        password = os.environ['DB_IMP_PASSWORD']
+    else:
+        server = os.environ['DB_IMP_TEST_HOST']
+        port = os.environ['DB_IMP_TEST_PORT']
+        schema = os.environ['DB_IMP_TEST_SCHEMA']
+        username = os.environ['DB_IMP_TEST_USER']
+        password = os.environ['DB_IMP_TEST_PASSWORD']
 
     connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
                                                                         server, port, schema)
@@ -19,17 +27,47 @@ def dba_import_login():
 
     return engine
 
-
-def dba_test_login():
+def dba_prod_tx(test=0):
     load_dotenv()
-
     dialect = "mysql"
     driver = "mysqlconnector"
-    username = os.environ['DB_TEST_USER']
-    password = os.environ['DB_TEST_PASSWORD']
-    server = os.environ['DB_TEST_HOST']
-    port = os.environ['DB_TEST_PORT']
-    schema = os.environ['DB_TEST_SCHEMA']
+
+    if test == 0:
+        server = os.environ['DB_TX_HOST']
+        port = os.environ['DB_TX_PORT']
+        schema = os.environ['DB_TX_SCHEMA']
+        username = os.environ['DB_TX_USER']
+        password = os.environ['DB_TX_PASSWORD']
+    else:
+        server = os.environ['DB_TX_TEST_HOST']
+        port = os.environ['DB_TX_TEST_PORT']
+        schema = os.environ['DB_TX_TEST_SCHEMA']
+        username = os.environ['DB_TX_TEST_USER']
+        password = os.environ['DB_TX_TEST_PASSWORD']
+
+    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
+                                                                        server, port, schema)
+    engine = create_engine(connection_string, poolclass=NullPool)
+
+    return engine
+
+def dba_prod_login(test=0):
+    load_dotenv()
+    dialect = "mysql"
+    driver = "mysqlconnector"
+
+    if test == 0:
+        server = os.environ['DB_PROD_HOST']
+        port = os.environ['DB_PROD_PORT']
+        schema = os.environ['DB_PROD_SCHEMA']
+        username = os.environ['DB_PROD_USER']
+        password = os.environ['DB_PROD_PASSWORD']
+    else:
+        server = os.environ['DB_PROD_TEST_HOST']
+        port = os.environ['DB_PROD_TEST_PORT']
+        schema = os.environ['DB_PROD_TEST_SCHEMA']
+        username = os.environ['DB_PROD_TEST_USER']
+        password = os.environ['DB_PROD_TEST_PASSWORD']
 
     connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
                                                                         server, port, schema)
@@ -37,37 +75,19 @@ def dba_test_login():
 
     return engine
 
-def db_prod_tx_login():
+def db_ana_login():
     load_dotenv()
-
     dialect = "mysql"
     driver = "mysqlconnector"
-    username = os.environ['DB_PROD_TX_USER']
-    password = os.environ['DB_PROD_TX_PASSWORD']
-    server = os.environ['DB_PROD_TX_HOST']
-    port = os.environ['DB_PROD_TX_PORT']
-    schema = os.environ['DB_PROD_TX_SCHEMA']
+
+    server = os.environ['DB_ANA_HOST']
+    port = os.environ['DB_ANA_PORT']
+    schema = os.environ['DB_ANA_SCHEMA']
+    username = os.environ['DB_ANA_USER']
+    password = os.environ['DB_ANA_PASSWORD']
 
     connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
                                                                         server, port, schema)
     engine = create_engine(connection_string)
 
     return engine
-
-def db_prod_login():
-    load_dotenv()
-
-    dialect = "mysql"
-    driver = "mysqlconnector"
-    username = os.environ['DB_PROD_USER']
-    password = os.environ['DB_PROD_PASSWORD']
-    server = os.environ['DB_PROD_HOST']
-    port = os.environ['DB_PROD_PORT']
-    schema = os.environ['DB_PROD_SCHEMA']
-
-    connection_string = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(dialect, driver, username, password,
-                                                                        server, port, schema)
-    engine = create_engine(connection_string)
-
-    return engine
-
